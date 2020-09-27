6aaab4d8afb932418960915f30710fa014c428e0
Noodulz <28810573+Noodulz@users.noreply.github.com>
2020-09-27 09:21:10 -0400

    Error handled for no results

--- src/movies.py
+++ src/movies.py
@@ -12,6 +12,7 @@ movie = Movie()
 
 def search(inputer):
     search = movie.search(inputer)
+    chosenID = 0
     n = 1
     if (len(search) > 5):
         print("\n")
@@ -20,6 +21,11 @@ def search(inputer):
             print(search[q].overview)
             n += 1
             print("\n")
+        num = int(input("Enter a number: "))
+        chosenID = search[num-1].id
+        return chosenID
+    elif(len(search) <= 0):
+        return chosenID
     else:
         print("\n")
         for r in range(0, len(search)):
@@ -27,22 +33,26 @@ def search(inputer):
             print(search[r].overview)
             n += 1
             print("\n")
-    num = int(input("Enter a number: "))
-    chosenID = search[num-1].id
-    return chosenID
+        num = int(input("Enter a number: "))
+        chosenID = search[num-1].id
+        return chosenID
 
 def main():
     userInput = input(colored("\nEnter a movie: ", 'green'))
     id = search(userInput)
-    recommendations = movie.recommendations(movie_id=id)
     movies = []
     time.sleep(0.5)
-    print("\n")
-    print(colored("Found!", 'red'))
-    print(colored("We recommended the following movies: \n", 'red'))
-    for i in range(0, 5):
-        print(colored(recommendations[i].title, 'magenta'))
-    userInput = input("\nPress Enter to Continue...")
+    if (id == 0):
+        print(colored("\nSorry! Could not find your recommendations or your results\n", 'magenta'))
+    else:
+        recommendations = movie.recommendations(movie_id=id)
+        print("\n")
+        print(colored("Found!", 'red'))
+        print(colored("We recommended the following movies: \n", 'red'))
+        for i in range(0, 5):
+            print(colored(recommendations[i].title, 'magenta'))
+        print("\n")
+    userInput = input("Press Enter to Continue...")
 
 if __name__ == '__main__':
     main()
